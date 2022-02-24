39906614c658f5c66e1125dd8db9ea0d596078c9
r-falcon <rfalcon@163.com>
2022-02-24 16:50:34 +0800

    feat: add code and git commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,22 @@
-/**
- * feat：新功能
- * upd：更新
- * del：删除
- * fix: 修复bug
- * refactor：结构上的优化（函数改个名字等）
- * test：新增或修改已有的测试代码
- * perf：优化了性能的代码改动
- * docs：更新的文档（例如更新readme）
- * style：样式美化（css修改缩进等）
- * revert：还原、恢复
- * chore：跟仓库主要业务⽆关的构建/⼯程依赖/⼯具等功能改动（⽐如新增⼀个⽂档⽣成⼯具）
- */
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
       2,
       'always',
-      ['feat', 'upd', 'del', 'fix', 'refactor', 'test', 'perf', 'docs', 'style', 'revert', 'chore'],
+      [
+        'feat', // 新功能
+        'fix', // 修补bug
+        'docs', // 文档变动
+        'style', // 格式变动（样式等不影响css的运行）
+        'refactor', // 重构（不新增，不修改bug的变动）
+        'perf', // 提高性能的代码变动
+        'test', // 增加测试
+        'chore', // 构建工程或辅助工具的变动
+        'revert' // 恢复以前的提交（回退、回滚）
+      ]
     ],
     'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-  },
-};
+    'subject-case': [0, 'never']
+  }
+}
